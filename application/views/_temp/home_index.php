c2f170deba4ab4db2cf4c1d06fff31b902a9d3f9
ariansyah-net <admin@ariansyah.net>
2020-08-26 11:33:25 +0700

    remove dokumentasi

--- application/views/_temp/home_index.php
+++ application/views/_temp/home_index.php
@@ -79,13 +79,9 @@
 <?php $this->load->view($main_view)?>
 <?php $this->load->view('_temp/home_footer')?>
 
-  <!-- JQuery -->
   <script type="text/javascript" src="<?=base_url('arians/home/js/jquery-3.4.1.min.js');?>"></script>
-  <!-- Bootstrap tooltips -->
   <script type="text/javascript" src="<?=base_url('arians/home/js/popper.min.js');?>"></script>
-  <!-- Bootstrap core JavaScript -->
   <script type="text/javascript" src="<?=base_url('arians/home/js/bootstrap.min.js')?>"></script>
-  <!-- MDB core JavaScript -->
   <script type="text/javascript" src="<?=base_url('arians/home/js/mdb.min.js')?>"></script>
   <script>new WOW().init();</script>
 </body>
